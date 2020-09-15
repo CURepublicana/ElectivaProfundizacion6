4d2a8cd9de904c4445567c0b4e3a052fa1144e78
Oscar Eduardo Ortiz Pinzon <oscardo.ortiz@gmail.com>
2020-09-14 20:54:09 -0500

    desarrollo
    desarrollo... Oscardo!

---
+++ Clase31DeAgosto/script/ejemplo.js
@@ -0,0 +1,9 @@
+//Ecma Javascript 6
+let variable = null;
+let variable1 = undefined;
+
+//variable con tipo de datos NULL que NO existe información
+//variable con tipo de datos UNDEFINED que existe información PERO NO SE HA LLENADO
+
+let CAJA = null;      //CONCEPTO 
+let CAJA = undefined; //REAL
